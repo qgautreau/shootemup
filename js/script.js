2feb3acd49ec4e28a1862df1d6b410775341b985
Quentin Gautreau <qgautreau@gmail.com>
2017-02-16 11:49:17 +0100

    cadre crée, création du pigeon en cours

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var playFrame = document.getElementById("playframe");
+var pigeon = document.createElement("img");
+pigeon.id = "pigeon";
+playFrame.appendChild("pigeon");
+
+pigeon.style.visibility="visible";
