a443c625351fcdb49a6f8c937ce3a4fca6fee040
Quentin Gautreau <qgautreau@gmail.com>
2017-02-16 15:10:07 +0100

    grosse merde

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 var playFrame = document.getElementById("playframe");
 var pigeon = document.createElement("img");
-pigeon.src = "./images/pigeon.jpg"
+pigeon.src = "./images/pigeon2.png"
 pigeon.id = "pigeon";
 playFrame.appendChild(pigeon);
 
-pigeon.style.left = Math.floor((Math.random()*80)+1) + '%';
-pigeon.style.top = Math.floor((Math.random()*80)+1) + '%';
+pigeon.style.left = Math.floor((Math.random()*90)+1) + '%';
+pigeon.style.top = Math.floor((Math.random()*90)+1) + '%';
+
+
+while (true) {
+    pigeon.style.left = Math.floor((Math.random()*90)+1) + '%';
+    pigeon.style.top = Math.floor((Math.random()*90)+1) + '%';
+};
+
+pigeon.addEventListener("click", function(){
+    pigeon.style.display = "none";
+});
